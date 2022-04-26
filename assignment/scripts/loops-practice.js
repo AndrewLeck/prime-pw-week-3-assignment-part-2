cdc00da8b2e24afab0587080bf51e9ebe7ef296b
Andrew Leck <Aleck360@gmail.com>
2022-04-26 00:36:54 -0500

    completed the rest of the questions

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -70,5 +70,20 @@ while( i < max ){
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+i = 0;
+max = 6;
+while( i < max ){
+  console.log(i);
+  i++;
+}
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+
+i = 10;
+low = 4;
+while( i > low){
+  console.log(i);
+  i--;
+}
